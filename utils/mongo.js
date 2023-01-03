e341fcf6535d22bbbbb51f3b4bad1f0466385fd3
Kanisshka <kanisshka12@gmail.com>
2023-01-03 21:34:49 +0530

    g

--- utils/mongo.js
+++ utils/mongo.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 const connectDb = async (DATABASE_URL)=>{
    try{
     const DB_OPTIONS ={
-        dbName: 'scrunchie',
+        dbName: 'unfiltered_connect',
     }
     await mongoose.connect(DATABASE_URL,DB_OPTIONS);
     console.log("connected");
