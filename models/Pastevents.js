32ff2cc3a3438108942b8d46aaf0463222ae6675
Rohan.27s <rohan.gotwal@graphia.in>
2023-01-12 18:42:42 +0530

    unique feature remove

--- models/Pastevents.js
+++ models/Pastevents.js
@@ -4,7 +4,6 @@ const PastSchema = new mongoose.Schema(
         title: {
             type: String,
             required: true,
-            unique: true,
         },
         content: {
             type: String,
